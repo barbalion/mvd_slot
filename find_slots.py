b7c130bd1009986594ea10a04ee7fab3bfc6abd4
Alexander Ermakov <barbalion@gmail.com>
2024-05-22 00:15:11 +0300

    improving blocking tolerance

--- find_slots.py
+++ find_slots.py
@@ -1,18 +1,25 @@
 import csv
+import json
 import multiprocessing
+import os
 import signal
+import time
 
 import requests
+from requests import JSONDecodeError
 
 service_id = 10001970310  # Получение паспорта нового поколения
 queue_id = 10000101388  # Unknown
 search_region = 45000000000  # OKATO: https://classifikators.ru/okato/45000000000
-org_url = f'https://www.gosuslugi.ru/api/nsi/v1/dictionary/MVD_equeue_{queue_id}'
+# org_url = f'https://www.gosuslugi.ru/api/nsi/v1/dictionary/MVD_equeue_{queue_id}'
+org_url = f'https://www.gosuslugi.ru/api/nsi/v1/dictionary/pgu_mvd_org'
 slots_url = 'https://www.gosuslugi.ru/api/lk/v1/equeue/agg/slots'
 headers_file = "headers.txt"
 
-num_threads = 30
-num_slots_to_print = 30
+num_threads = 5
+throttle_delay_sec = 1
+num_slots_to_print = 50
+block_sleep_time_sec = 10
 
 
 def read_headers(filename):
@@ -25,6 +32,8 @@ def read_headers(filename):
 
 
 def org_req(region):
+    if os.path.exists("org_request.json"):
+        return json.load(open("org_request.json"))
     return {
         "filter": {"union": {"unionKind": "AND", "subs": [
             {"simple": {"attributeName": "region", "condition": "EQUALS",
@@ -47,21 +56,41 @@ def read_orgs(url=org_url):
     print('Found', len(items), 'orgs.')
     for o in items:
         v = o['attributeValues']
-        yield Org(v['address'], v['code'], v['SLOTPERCENT'], v['okato'])
+        yield Org(
+            v.get('address') or v.get('address_out'),
+            v.get('code') or v.get('CODE_FRGU'),
+            v.get('SLOTPERCENT'), v.get('okato'),
+        )
 
 
-def find_slots(org):
-    try:
-        print(f'Reading slots for {org}...')
-        req = {"organizationId": [str(org)], "serviceId": [str(service_id)], "eserviceId": str(queue_id),
-               "attributes": [], "filter": None}
-        response = requests.post(slots_url, json=req, headers=read_headers(headers_file))
-        response.raise_for_status()
-        slots_data = response.json()
-        return list(sorted([s['visitTime'] for s in slots_data['slots']]))
-    except Exception as e:
-        print(f'Error reading slots for {org}: {e}')
-        return [f'error']
+def handler(*_):
+    raise Exception('Interrupted.')
+
+
+def find_slots(org, start_time=None):
+    signal.signal(signal.SIGINT, handler)
+    sleep_time = start_time - time.time()
+    if start_time and sleep_time > 0:
+        time.sleep(sleep_time)
+
+    while True:
+        try:
+            req = {"organizationId": [str(org)], "serviceId": [str(service_id)], "eserviceId": str(queue_id),
+                   "attributes": [], "filter": None}
+            response = requests.post(slots_url, json=req, headers=read_headers(headers_file))
+            response.raise_for_status()
+            slots_data = response.json()
+            slots = list(sorted([s['visitTime'] for s in slots_data['slots']]))
+            print(f'Found {len(slots)} slots for {org}...')
+            if sleep_time < 0:
+                time.sleep(throttle_delay_sec)
+            return slots
+        except JSONDecodeError as e:
+            print(f'We looks to be blocked "{e}"! Refresh headers! Sleeping {block_sleep_time_sec} seconds...')
+            time.sleep(block_sleep_time_sec)
+        except Exception as e:
+            print(f'Error reading slots for {org}: {e}')
+            return [f'error']
 
 
 class Org:
@@ -81,7 +110,7 @@ def write_output(orgs):
     with open('orgs_data.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(
-            ['Code', 'Address', 'Slot Percentage', 'Slots count', *[f'Slot{n+1}' for n in range(num_slots_to_print)]])
+            ['Code', 'Address', 'Slot Percentage', 'Slots count', *[f'Slot{n + 1}' for n in range(num_slots_to_print)]])
         for org in orgs:
             writer.writerow(org.get_row())
 
@@ -91,12 +120,17 @@ def main():
     original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
     pool = multiprocessing.Pool(processes=num_threads)
     signal.signal(signal.SIGINT, original_sigint_handler)
-    orgs = list(read_orgs())
-    slots = list(pool.map(find_slots, [o.code for o in orgs]))
-    for o, s in zip(orgs, slots):
-        o.slots = s
-    write_output(orgs)
-    print(f'Done.')
+    try:
+        orgs = list(read_orgs())
+        job_params = list([(o.code, time.time() + i * throttle_delay_sec) for i, o in enumerate(orgs)])
+        slots = list(pool.starmap(find_slots, job_params))
+        for o, s in zip(orgs, slots):
+            o.slots = s
+        write_output(orgs)
+        print(f'Done.')
+    except (KeyboardInterrupt, SystemExit):
+        pool.terminate()
+        pool.join()
 
 
 if __name__ == "__main__":
